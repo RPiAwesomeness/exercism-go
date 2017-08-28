c1ca9a5049748e74a2a6e0b101dc658140ff0a3a
RPiAwesomeness <keyboardmailtesting@gmail.com>
2017-08-28 14:40:32 -0400

    Making progress

--- clock/clock.go
+++ clock/clock.go
@@ -15,46 +15,50 @@ type Clock struct {
 
 // New creates a new Clock object and returns it from the given time in hours & minutes
 func New(hour, minute int) Clock {
+	var createHour, createMin int
 	// TODO: Add logic for handling roll-over, continuous roll-over, and negatives
 	// Midnight is {0, 0}
 	// Negative hours are from {0, 0}
 	// 60 minutes = 1 hour (minutes roll over into hours)
 
 	// Parse the minute value
-	if minute > 0 && minute < 60 {
-
-	} else if minute >= 60 {
+	if minute >= 60 {
 		// It's greater than 60, roll over into X hours
-		hour += minute / 60
-	} else if minute < 0 && minute > -60 {
-		// It's negative щ(゜ロ゜щ)
-		// Subtract 1 hour
-		hour--
-		// Set minute to a postive value < 60
-		minute = 60 - minute
-	} else if minute <= -60 {
+		createHour += minute / 60
+		// Subtract that many hours worth of minutes
+		createMin = minute - (createHour * 60)
+	} else if minute <= 0 {
 		// It's negative щ(゜ロ゜щ)
 		// += because it'll be a negative number from the division
-		hour += minute / 60
+		createHour += minute / 60
+		// Subtract the number of hours in minutes from the minutes
+		createMin = minute - (createHour * 60)
+	} else {
+		// It's a positive minute less than an hour
+		createMin = minute
 	}
 
 	// Parse the hour values
-	if hour > 0 {
-		// It's a number > 0, thus it's a normal hour
-	} else if hour == 24 {
-		// It's midnight
-		hour = 0
-	} else if hour > 24 {
-		// It's more than 1 day, roll over logic here
-	} else {
+	if hour >= 24 {
+		// It's more than 1 day, roll over
+		// How many days?
+		days := hour / 24
+		// Remove that many days worth of hours
+		createHour -= hour - (days * 24)
+	} else if hour <= 0 {
 		// It's a negative hour! щ(゜ロ゜щ) Roll over backwards
-		hour = 24 - hour
+		days := hour / 24
+		// Remove that many days worth of hours
+		createHour -= hour + (days * 24)
+	} else {
+		// It's a positive hour less than a day
+		createHour += hour
 	}
 
 	// Set up the Clock object and return it
 	return Clock{
-		Hour:   hour,
-		Minute: minute,
+		Hour:   createHour,
+		Minute: createMin,
 	}
 }
 
